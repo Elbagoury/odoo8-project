20ea613fc826076068b30bcad43955c9881e6dbd
muhammad105 <ta.awaisajaz@gmail.com>
2018-01-22 20:16:44 +0500

    google api odoo v8

---
+++ vat/google_api_integreation_ov8/__openerp__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Google Docs Integration', 
+	'description': 'Google Docs Integration: This Module lets you to develop,'\
+	 'read and modify Spreadsheet, Document, Presentation.',
+	'author': 'Muhammad Awais',
+	'depends':['base','project'], 
+	'application': True,
+	'data': ['views/template.xml','security/security.xml','security/ir.model.access.csv'],
+}
